fc96d48eea98c7f7fe26caf9763ae9ecfd754744
atulrana007 <atul.rana@zopsmart.com>
2021-11-08 11:26:14 +0530

    updated according to custom-db

--- src/providers/AccountContext.jsx
+++ src/providers/AccountContext.jsx
@@ -16,24 +16,24 @@ const AccountProvider = (props) => {
     localStorage.setItem("userData", JSON.stringify(data));
   };
   console.log("configs", props);
-  // const webAuth = new auth0.WebAuth({
-  //   domain: props.config.auth0Domain,
-  //   clientID: props.config.clientID,
-  //   redirectUri: props.config.callbackURL,
-  //   responseType: props.config.extraParams.response_type,
-  //   scope: props.config.extraParams.scope,
-  //   state: props.config.extraParams.state,
-  //   nonce: props.config.extraParams.nonce,
-  //   _csrf: props.config.extraParams._csrf,
-  //   audience: props.config.extraParams.audience,
-  //   overrides: { __tenant: props.config.auth0Tenant },
-  // });
   const webAuth = new auth0.WebAuth({
-    domain: process.env.REACT_APP_AUTH0_DOMAIN,
-    clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
-    responseType: "token id_token",
-    redirectUri: "http://localhost:4040/authorize",
+    domain: props.config.auth0Domain,
+    clientID: props.config.clientID,
+    redirectUri: props.config.callbackURL,
+    responseType: props.config.extraParams.response_type,
+    scope: props.config.extraParams.scope,
+    state: props.config.extraParams.state,
+    nonce: props.config.extraParams.nonce,
+    _csrf: props.config.extraParams._csrf,
+    audience: props.config.extraParams.audience,
+    overrides: { __tenant: props.config.auth0Tenant },
   });
+  // const webAuth = new auth0.WebAuth({
+  //   domain: process.env.REACT_APP_AUTH0_DOMAIN,
+  //   clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
+  //   responseType: "token id_token",
+  //   redirectUri: "http://localhost:4040/authorize",
+  // });
   const getSocialLogin = (name) => {
     return new Promise((resolve, reject) => {
       webAuth.authorize(
@@ -58,8 +58,7 @@ const AccountProvider = (props) => {
     };
     return new Promise((resolve, reject) => {
       const variables = {
-        // connection: "Test-CustomDB",
-        connection: "Username-Password-Authentication",
+        connection: "Test-CustomDB",
         email,
         password,
         userMetadata,
@@ -113,7 +112,7 @@ const AccountProvider = (props) => {
       webAuth.login(
         {
           // realm: "Test-CustomDB",
-          realm: "Username-Password-Authentication",
+          realm: "Test-CustomDB",
           username,
           password,
           onRedirecting: function (done) {
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     main: lModules,
   },
   output: {
-    filename: "bundle_1_0.min.js",
+    filename: "bundle_01.min.js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
